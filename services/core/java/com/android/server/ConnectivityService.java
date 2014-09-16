525933b1f10453d50e492284b7daae607827caed
Nalla Kartheek <karthe@codeaurora.org>
2014-09-16 10:25:36 +0800

    SOFTAP: implement SOFTAP new requirement in framework.
    If framework recevied the STA (dis)connect message from netd,
    it will try to read device info from dnsmasq and broadcast
    an state change event via intent.
    Meanwhile, framework will maintain the conneted Sta list which
    can be access from upper layer
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Change-Id: I14972a7294e76ee97f9dce78a474b302227328f0
    
    Conflicts:
    	core/res/res/values/symbols.xml

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -71,6 +71,7 @@ import android.net.ProxyInfo;
 import android.net.RouteInfo;
 import android.net.UidRange;
 import android.net.Uri;
+import android.net.wifi.WifiDevice;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.FileUtils;
@@ -2590,6 +2591,14 @@ public class ConnectivityService extends IConnectivityManager.Stub
                     break;
                 }
             }
+        }
+    }
+
+    public List<WifiDevice> getTetherConnectedSta() {
+        if (isTetheringSupported()) {
+            return mTethering.getTetherConnectedSta();
+        } else {
+            return null;
         }
     }
 
