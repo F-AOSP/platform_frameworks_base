CyanogenMod/android_frameworks_base

power: Add CPU boosting interface
 * Boosts CPU using Power HAL for the given duration.
 * Duration is given in microseconds.
 * Power HAL must implement POWER_HINT_CPU_BOOST.

Change-Id: Ic79baf7e3d0f75483c2fe8a242b4c3d93368b68b

Add CPU boosting hooks

 * Send boost hint during scrolling
 * Still relevant on even high-end hardware

Change-Id: I72f2c2851b01fbceabddead03a4c3a7a0526ca61

perf: Send a boost hint when a key on the navbar is pressed

 * A lot of stuff happens, especially when invoking recents.
 * Get ahead of the storm by sending a boost hint before
   kicking off the animations.

Change-Id: I0cecd0c58f8bc8479ea44f63af796500d7b9d7d2

perf: Send boost hint during initial launch

Change-Id: I26afc909c35e514958894b0a07a650a20ce6e537

services/core/java/com/android/server/power/PowerManagerService.java
@@ -159,6 +159,9 @@ public final class PowerManagerService extends SystemService
     // Config value for NSRM
     private static final int DPM_CONFIG_FEATURE_MASK_NSRM = 0x00000004;
 
+    // Max time (microseconds) to allow a CPU boost for
+    private static final int MAX_CPU_BOOST_TIME = 5000000;
+
     private final Context mContext;
     private final ServiceThread mHandlerThread;
     private final PowerManagerHandler mHandler;
@@ -439,6 +442,7 @@ public final class PowerManagerService extends SystemService
     private static native void nativeSetInteractive(boolean enable);
     private static native void nativeSetAutoSuspend(boolean enable);
     private static native void nativeSendPowerHint(int hintId, int data);
+    private static native void nativeCpuBoost(int duration);
 
     public PowerManagerService(Context context) {
         super(context);
@@ -3028,6 +3032,21 @@ public final class PowerManagerService extends SystemService
                 Binder.restoreCallingIdentity(ident);
             }
         }
+
+        /**
+         * Boost the CPU
+         * @param duration Duration to boost the CPU for, in milliseconds.
+         * @hide
+         */
+        @Override
+        public void cpuBoost(int duration) {
+            if (duration > 0 && duration <= MAX_CPU_BOOST_TIME) {
+                nativeCpuBoost(duration);
+            } else {
+                Log.e(TAG, "Invalid boost duration: " + duration);
+            }
+        }
+
 
         /**
          * Reboots the device.

F-AOSP/platform_frameworks_base

Conflicts:
	tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java

services/core/java/com/android/server/power/PowerManagerService.java
@@ -154,6 +154,9 @@ public final class PowerManagerService extends SystemService
     // Config value for NSRM
     private static final int DPM_CONFIG_FEATURE_MASK_NSRM = 0x00000004;
 
+    // Max time (microseconds) to allow a CPU boost for
+    private static final int MAX_CPU_BOOST_TIME = 5000000;
+
     private final Context mContext;
     private final ServiceThread mHandlerThread;
     private final PowerManagerHandler mHandler;
@@ -442,6 +445,7 @@ public final class PowerManagerService extends SystemService
     private static native void nativeSetInteractive(boolean enable);
     private static native void nativeSetAutoSuspend(boolean enable);
     private static native void nativeSendPowerHint(int hintId, int data);
+    private static native void nativeCpuBoost(int duration);
 
     public PowerManagerService(Context context) {
         super(context);
@@ -3084,6 +3088,21 @@ public final class PowerManagerService extends SystemService
                 Binder.restoreCallingIdentity(ident);
             }
         }
+
+        /**
+         * Boost the CPU
+         * @param duration Duration to boost the CPU for, in milliseconds.
+         * @hide
+         */
+        @Override
+        public void cpuBoost(int duration) {
+            if (duration > 0 && duration <= MAX_CPU_BOOST_TIME) {
+                nativeCpuBoost(duration);
+            } else {
+                Slog.e(TAG, "Invalid boost duration: " + duration);
+            }
+        }
+
 
         /**
          * Reboots the device.