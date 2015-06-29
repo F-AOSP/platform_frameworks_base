8d627ee3f6e24554b9e0b2ba0cf2a866d9e54210
tiger_huang <tiger_huang@htc.com>
2015-06-29 17:17:30 +0800

    Use the correct lock to protect members in PolicyControl
    Members in PolicyControl can be accessed by multiple threads via the
    public functions called by PhoneWindowManager. Most of the functions
    are called with the window-manager-lock held, but reloadFromSetting()
    is an exception.
    
    In this patch, we acquire the window-manager-lock before calling
    reloadFromSetting() to ensure accessing the members with the same
    lock held.
    
    Change-Id: I54ac5cab2a2d74c568fa96145f7a6741c6d99e36

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1579,6 +1579,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (mImmersiveModeConfirmation != null) {
                 mImmersiveModeConfirmation.loadSetting(mCurrentUserId);
             }
+        }
+        synchronized (mWindowManagerFuncs.getWindowManagerLock()) {
             PolicyControl.reloadFromSetting(mContext);
         }
         if (updateRotation) {
