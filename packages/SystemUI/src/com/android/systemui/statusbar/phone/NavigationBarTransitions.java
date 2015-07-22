4cf2b362fecfe905bb8981d6a9049a9d5e9ac848
Steve Kondik <steve@cyngn.com>
2015-07-22 12:45:39 -0700

    systemui: Fix various issues with transparent bars
     * You current can't enable lights-out with transparency/translucency-
       the code is only half-way there but it's sequenced in a strange way
       and the translucent combo isn't implemented at all. Fix it.
     * Enabling a transparent navbar when the bar is vertical (landscape)
       has bugs that make the behavior unpredictable for some apps. We can
       still allow it, so add a new View flag to signal this.
    
    Change-Id: Iec98c960d6c13a294d8f9f0dd9f9e2fc1f8f9b52

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
@@ -38,6 +38,7 @@ public final class NavigationBarTransitions extends BarTransitions {
     private boolean mLightsOut;
     private boolean mVertical;
     private int mRequestedMode;
+    private boolean mTransparencyAllowedWhenVertical = false;
 
     public NavigationBarTransitions(NavigationBarView view) {
         super(view, R.drawable.nav_background);
@@ -57,14 +58,18 @@ public final class NavigationBarTransitions extends BarTransitions {
         transitionTo(mRequestedMode, false /*animate*/);
     }
 
+    public void setTransparencyAllowedWhenVertical(boolean allowed) {
+        mTransparencyAllowedWhenVertical = allowed;
+    }
+
     @Override
     public void transitionTo(int mode, boolean animate) {
         mRequestedMode = mode;
-        if (mVertical) {
+        if (mVertical && !mTransparencyAllowedWhenVertical) {
             // translucent mode not allowed when vertical
             if (mode == MODE_TRANSLUCENT || mode == MODE_TRANSPARENT) {
                 mode = MODE_OPAQUE;
-            } else if (mode == MODE_LIGHTS_OUT_TRANSPARENT) {
+            } else if (mode == MODE_LIGHTS_OUT_TRANSPARENT || mode == MODE_LIGHTS_OUT_TRANSLUCENT) {
                 mode = MODE_LIGHTS_OUT;
             }
         }
