e7ca51b5fa29c68a877ed10ddaaaa8bbda996f32
Steve Kondik <steve@cyngn.com>
2015-07-25 19:06:49 -0700

    wm: Underp WindowManager flags
     * One was duplicated, the rest were out of order. Fixit.
    
    Change-Id: I401251bf3a0c19b16217dde1bdf66ec1b31651b2

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1101,16 +1101,16 @@ public interface WindowManager extends ViewManager {
          */
         public static final int PRIVATE_FLAG_KEYGUARD = 0x00000400;
 
-        /** Window flag: mark layer as fully transparent
-         * {@hide} */
-        public static final int PRIVATE_FLAG_FULLY_TRANSPARENT = 0x10000000;
-
         /**
          * Flag that prevents the wallpaper behind the current window from receiving touch events.
          *
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_DISABLE_WALLPAPER_TOUCH_EVENTS = 0x00000800;
+        public static final int PRIVATE_FLAG_DISABLE_WALLPAPER_TOUCH_EVENTS = 0x00004000;
+
+        /** Window flag: mark layer as fully transparent
+         * {@hide} */
+        public static final int PRIVATE_FLAG_FULLY_TRANSPARENT = 0x10000000;
 
         /**
          * Control flags that are private to the platform.
