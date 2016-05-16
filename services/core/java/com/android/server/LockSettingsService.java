11ea1ae00ebfe26b1bc17c506dd5c48b521dcc4a
AnilKumar Chimata <anilc@codeaurora.org>
2016-05-16 15:32:16 +0530

    frameworks/base: Fix pattern update issue
    Fix pattern update issue while changing the user pin/password/
    pattern.
    
    Change-Id: Ie7461eef4cc82a1f591a50d094e47c586cc24198

--- services/core/java/com/android/server/LockSettingsService.java
+++ services/core/java/com/android/server/LockSettingsService.java
@@ -1250,7 +1250,10 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public VerifyCredentialResponse checkPattern(String pattern, int userId,
             ICheckCredentialProgressCallback progressCallback) throws RemoteException {
-        return doVerifyPattern(pattern, false, 0, userId, progressCallback);
+        VerifyCredentialResponse response = doVerifyPattern(pattern, false, 0, userId, progressCallback);
+        if (response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK)
+            retainPassword(pattern);
+        return response;
     }
 
     @Override
@@ -1314,8 +1317,6 @@ public class LockSettingsService extends ILockSettings.Stub {
                && shouldReEnrollBaseZero) {
             setLockPatternInternal(pattern, patternToVerify, userId);
        }
-       if (response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK)
-           retainPassword(pattern);
 
        return response;
     }
