Jackeagle/caf_frameworks_base

Show "Sign-in network" notification with correct operator name.
Use getNetworkOperatorName specifically with the DDS sub Id to
get and display the correct operator name in the "sign in to network"
notification.

Change-Id: Id6a27ec2f3c3f1a165f9a36d88b8359774b428ca
CRs-Fixed: 1012068

services/core/java/com/android/server/ConnectivityService.java
@@ -93,6 +93,7 @@ import android.os.UserManager;
 import android.provider.Settings;
 import android.security.Credentials;
 import android.security.KeyStore;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.LocalLog;
@@ -3339,7 +3340,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
                         title = r.getString(R.string.network_available_sign_in, 0);
                         // TODO: Change this to pull from NetworkInfo once a printable
                         // name has been added to it
-                        details = mTelephonyManager.getNetworkOperatorName();
+                        details = mTelephonyManager.getNetworkOperatorName(
+                             SubscriptionManager.getDefaultDataSubId());
                         icon = R.drawable.stat_notify_rssi_in_range;
                         break;
                     default:

F-AOSP/platform_frameworks_base

services/core/java/com/android/server/ConnectivityService.java
@@ -99,6 +99,7 @@ import android.os.UserManager;
 import android.provider.Settings;
 import android.security.Credentials;
 import android.security.KeyStore;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.ArraySet;
@@ -3511,7 +3512,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
                         title = r.getString(R.string.network_available_sign_in, 0);
                         // TODO: Change this to pull from NetworkInfo once a printable
                         // name has been added to it
-                        details = mTelephonyManager.getNetworkOperatorName();
+                        details = mTelephonyManager.getNetworkOperatorName(
+                             SubscriptionManager.getDefaultDataSubscriptionId());
                         icon = R.drawable.stat_notify_rssi_in_range;
                         break;
                     default: